5ee23c8113c933f999c849117fc0aefa9ff56129
Audrey Tang <audreyt@audreyt.org>
2015-04-04 23:03:10 +0800

    * Generate bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,33 @@
 var path = require('path');
 var webpack = require('webpack');
+var plugins = [];
+var entries = [ './scripts/index' ];
+var loaders = [ 'babel?stage=0' ];
+if (/production/.test(process.env.NODE_ENV)) {
+    plugins = [ new webpack.optimize.UglifyJsPlugin() ];
+}
+else {
+    plugins = [ new webpack.HotModuleReplacementPlugin(), new webpack.NoErrorsPlugin() ];
+    entries.push('webpack-dev-server/client?http://localhost:3000', 'webpack/hot/only-dev-server');
+    loaders.unshift('react-hot');
 
+}
 module.exports = {
   devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './scripts/index'
-  ],
+  entry: entries,
   output: {
-    path: path.join(__dirname, 'build'),
+    path: path.join(__dirname, 'scripts'),
     filename: 'bundle.js',
-    publicPath: '/scripts/'
+    publicPath: './scripts/'
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
+  plugins: plugins,
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
   module: {
     loaders: [{
       test: /\.jsx?$/,
-      loaders: ['react-hot', 'babel?stage=0'],
+      loaders: loaders,
       include: path.join(__dirname, 'scripts')
     }]
   }
